984f8dd810579365b6d8d5258f87b96277cce940
Rixtinator <129871232+Rixtinator@users.noreply.github.com>
2023-05-25 11:34:13 +0200

    functions to add, substract, multiply and divide numbers

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+};
+
+console.log(add(5, 5));
+
+function subtract(a, b) {
+    return a - b;
+};
+
+console.log(subtract(10, 4));
+
+function multiply(a, b) {
+    return a * b;
+};
+
+console.log(multiply(3, 5));
+
+function divide(a, b) {
+    return a / b;
+};
+
+console.log(divide(10, 3));
