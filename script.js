5c52a60184fe77eacc3505deff5f9bb5fcc0873f
Rixtinator <129871232+Rixtinator@users.noreply.github.com>
2023-06-01 10:45:58 +0200

    add if else statements when pushed button dot or pressed key dot so that the user can only place one dot per number.

--- script.js
+++ script.js
@@ -20,80 +20,7 @@ function showDisplay() {
     } else { displaySum.textContent = firstNumber + clickedOperator + storedNumber };
 };
 
-function clear() {
-    storedNumber = '';
-    firstNumber = '';
-    clickedOperator = '';
-    displaySum.textContent = '';
-    displayResult.textContent = '';
-};
-
-function calculate() {
-    result = operate(parseFloat(firstNumber), parseFloat(storedNumber), clickedOperator);
-    displayResult.textContent = Math.round(result * 1000000) / 1000000;
-    storedNumber = Math.round(result * 1000000) / 1000000;
-};
-
-function addOperator() {
-    firstNumber = storedNumber;
-    displaySum.textContent += clickedOperator;
-    storedNumber = '';
-};
-
-function del() {
-    storedNumber = storedNumber.slice(0, -1);
-    showDisplay();
-}
-
-numberButton.forEach(function (number) {
-    number.addEventListener('click', () => {
-        storedNumber += number.value;
-        showDisplay();
-    });
-});
-
-operatorButton.forEach(function (operator) {
-    operator.addEventListener('click', () => {
-        clickedOperator = operator.value;
-        addOperator();
-    });
-});
-
-equalsButton.addEventListener('click', () => {
-    calculate();
-});
-
-
-clearButton.addEventListener('click', () => {
-    clear();
-});
-
-deleteButton.addEventListener('click', () => {
-    del();
-});
-
-window.addEventListener("keydown", (event) => {
-    const key = event.key
-    if (/[0-9.]/.test(key)) {
-        storedNumber += key;
-        showDisplay();
-    };
-    if (/[+,/,*,-]/.test(key)) {
-        clickedOperator = key;
-        addOperator();
-        return;
-    };
-    if (/[=]/.test(key) || /[/r]/.test(key)) {
-        calculate();
-    };
-    if (/^Backspace$/i.test(key)) {
-        del();
-    };
-    if (/c$/.test(key) || /[C]/.test(key)) {
-        clear();
-    };
-});
-
+//Calculate functions
 function add(firstNumber, nextNumber) {
     return firstNumber + nextNumber;
 };
@@ -122,3 +49,104 @@ function operate(firstNumber, nextNumber, clickedOperator) {
             return divide(firstNumber, nextNumber);
     };
 };
+
+function clear() {
+    storedNumber = '';
+    firstNumber = '';
+    clickedOperator = '';
+    displaySum.textContent = '';
+    displayResult.textContent = '';
+    dot = false;
+};
+
+function calculate() {
+    result = operate(parseFloat(firstNumber), parseFloat(storedNumber), clickedOperator);
+    displayResult.textContent = Math.round(result * 1000000) / 1000000;
+    storedNumber = Math.round(result * 1000000) / 1000000;
+    dot = false;
+};
+
+function addOperator() {
+    firstNumber = storedNumber;
+    displaySum.textContent += clickedOperator;
+    storedNumber = '';
+    dot = false;
+};
+
+function del() {
+    storedNumber = storedNumber.slice(0, -1);
+    showDisplay();
+};
+
+// Button input
+let dot = false;
+
+function numberInput() {
+
+}
+
+numberButton.forEach(function (number) {
+    number.addEventListener('click', () => {
+        if (number.value === '.' && dot === false) {
+            storedNumber += number.value;
+            showDisplay();
+            return dot = true;
+        } else if (number.value === '.' && dot === true) {
+            return;
+        } else {
+            storedNumber += number.value;
+            showDisplay();
+        };
+    });
+});
+
+operatorButton.forEach(function (operator) {
+    operator.addEventListener('click', () => {
+        clickedOperator = operator.value;
+        addOperator();
+    });
+});
+
+equalsButton.addEventListener('click', () => {
+    calculate();
+});
+
+
+clearButton.addEventListener('click', () => {
+    clear();
+});
+
+deleteButton.addEventListener('click', () => {
+    del();
+});
+
+//Keyboard input
+window.addEventListener("keydown", (event) => {
+    const key = event.key
+    if (/[0-9.]/.test(key)) {
+        if (key === '.' && dot === false) {
+            storedNumber += key;
+            showDisplay();
+            return dot = true;
+        } else if (key === '.' && dot === true) {
+            return;
+        } else {
+            storedNumber += key;
+            showDisplay();
+        };
+    };
+    if (/[+,/,*,-]/.test(key)) {
+        clickedOperator = key;
+        addOperator();
+        return;
+    };
+    if (/[=]/.test(key) || /[/r]/.test(key)) {
+        calculate();
+    };
+    if (/^Backspace$/i.test(key)) {
+        del();
+    };
+    if (/c$/.test(key) || /[C]/.test(key)) {
+        clear();
+    };
+});
